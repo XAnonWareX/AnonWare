fa5410f7054af8755a44d108c1e9148423b44179
XAnonWareX <AnonWare22@outlook.com>
2024-05-31 06:22:04 -0700

    Add files via upload

---
+++ serial_checker.bat
@@ -0,0 +1,19 @@
+ECHO OFF
+TITLE HWID Checker
+ECHO **********************************
+Color 04
+ECHO **********************************
+:start
+cls
+ECHO CPU 
+wmic cpu get serialnumber
+ECHO BIOS
+wmic bios get serialnumber
+ECHO Motherboard
+wmic baseboard get serialnumber
+ECHO smBIOS UUID
+wmic path win32_computersystemproduct get uuid
+getmac
+echo Press any key to get your hardware serials again.
+pause>nul
+goto start
